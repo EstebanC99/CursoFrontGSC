f2b8de162b2b00e63d39c362e3a62276fe615737
Dunge0N3 <estebancarignani99@gmail.com>
2022-09-20 18:07:18 -0300

    Ejercicio 13 de Angular

---
+++ PracticaAngular/ejercicio13/src/app/events-list/events-list.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+
+interface IEvent {
+  format?: string;
+}
+
+enum EventFormat {
+  InPerson = 'InPerson',
+  Online = 'Online'
+}
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: ['./events-list.component.css']
+})
+export class EventsListComponent implements OnInit {
+
+  events = [
+    {
+      name: 'Angular Connect',
+      time: '10am',
+      location: {
+        address: '1 London Road ',
+        city: 'London',
+        country: 'England'
+      },
+      format: "InPerson"
+    },
+    {
+      name: 'ng-conf 2037',
+      date: '4/15/2037',
+      time: '9am',
+      onlineUrl: 'https://www.ng-conf.org/',
+      format: "Online"
+    },
+    {
+      name: 'Future Conf (Location/Url a determinar)',
+      date: '6/10/2037',
+      time: '8am'
+    },
+    {
+      name: 'UN Angular Summit',
+      date: '6/10/2037',
+      time: '8am',
+      location: {
+        address: 'The UN Angular Center',
+        city: 'New York',
+        country: 'USA'
+      },
+      format: "InPerson"
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getClassFormat(event: IEvent): string {
+    let classToAssign: string = "adeterminar";
+
+    switch (event.format) {
+      case EventFormat.InPerson:
+        classToAssign = "in-person";
+        break;
+      case EventFormat.Online:
+        classToAssign = "online"
+        break;
+      default:
+        break;
+    }
+    return classToAssign;
+  }
+
+}
